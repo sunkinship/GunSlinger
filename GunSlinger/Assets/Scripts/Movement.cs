ab6aa4b173785f4eaf47625464c56840df301df9
Leo <leo_1517@outlook.com>
2021-06-14 02:02:00 -0700

    Added Movement

---
+++ GunSlinger/Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    
+    public float scalar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posX = Time.deltaTime * scalar * Input.GetAxis("Horizontal");
+        float posY = Time.deltaTime * scalar * Input.GetAxis("Vertical");
+        transform.position += new Vector3(posX, posY, transform.position.z);
+    }
+}
---
+++ GunSlinger/Assets/Scripts/Movement2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2 : MonoBehaviour
+{
+    
+    public float scalar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posX = Time.deltaTime * scalar * Input.GetAxis("Horizontal2");
+        float posY = Time.deltaTime * scalar * Input.GetAxis("Vertical2");
+        transform.position += new Vector3(posX, posY, transform.position.z);
+    }
+}
