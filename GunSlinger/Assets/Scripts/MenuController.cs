51c631aabfb19d8cf6512a5fbcb78810d61d941e
Leo <leo_1517@outlook.com>
2021-06-15 02:06:53 -0700

    Added main menu and text font

---
+++ GunSlinger/Assets/Scripts/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void ButtonPlay()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void ButtonCredits()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ButtonRules()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ButtonQuit()
+    {
+        Application.Quit();
+    }
+}
